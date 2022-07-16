c20ec83a2ac47452d608e2ef123bcea371410bc0
Sanidhya Kr. Verma <sanidhya.verma12345@gmail.com>
2022-07-16 14:43:09 +0530

    tailwindcss setup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,11 @@
-import type { NextPage } from 'next'
+import type { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
     <div>
-      <h1>Tiktok Clone</h1>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
