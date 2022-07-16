f8ab7d48eb2f7e83d08fe41e5d1f798d79c8f763
Sanidhya Kr. Verma <sanidhya.verma12345@gmail.com>
2022-07-16 15:05:57 +0530

    navbar created

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,27 @@
 import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { GoogleLogin, GoogleLogout } from "react-google-login";
+import { AiOutlineLogout } from "react-icons/ai";
+import { BiSearch } from "react-icons/bi";
+import { IoMdAdd } from "react-icons/io";
+
+import Logo from "../utils/tiktik-logo.png";
 
 const Navbar = () => {
   return (
-    <div>
-      <h1>Navbar</h1>
+    <div className="w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4">
+      <Link href="/">
+        <div className="w-[100px] md:w-[130px]">
+          <Image
+            className="cursor-pointer"
+            src={Logo}
+            alt="TikTik"
+            layout="responsive"
+          />
+        </div>
+      </Link>
     </div>
   );
 };
