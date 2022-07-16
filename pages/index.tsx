cbcc1f79884ea5bce096677e5e033b8ff292740b
Sanidhya Kr. Verma <sanidhya.verma12345@gmail.com>
2022-07-16 14:56:08 +0530

    components created

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,7 @@
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
-  return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </div>
-  );
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 };
 
 export default Home;
