c86d1c3387a756c96f73bc396c5dcc7676c4ec77
Sanidhya Kumar Verma <sanidhyyy@gmail.com>
2026-09-15 19:34:09 +0530

    chore(deps): use pnpm v11

--- next.config.js
+++ next.config.js
@@ -4,7 +4,6 @@ const nextConfig = {
     ignoreBuildErrors: true,
   },
   reactStrictMode: true,
-  swcMinify: true,
   images: {
     unoptimized: true,
     domains: ["i.imgur.com", "lh3.googleusercontent.com"],
