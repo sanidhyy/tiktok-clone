d3ff67e23cb037fbc161e42063c64b054fcf8323
Sanidhya Kumar Verma <sanidhyyy@gmail.com>
2026-09-15 21:31:06 +0530

    chore: hide dev indicators

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   agentRules: false,
+  devIndicators: false,
   images: {
     unoptimized: true,
     remotePatterns: [
